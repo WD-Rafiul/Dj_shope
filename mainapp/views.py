b3b5869c292dcf63392db3841b2ba68c6a64bf89
WD-Rafiul <rafiulislam331@gmail.com>
2023-08-25 10:43:01 +0600

    image rendering ok

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,5 +4,6 @@ from item.models import Category, Items
 
 # Create your views here.
 def index(request):
-    item = Items.objects.filter(is_sold=False)
-    return render(request, 'mainapp/index.html')
+    items = Items.objects.filter(is_sold=False)[:12]
+    categories = Category.objects.all()
+    return render(request, 'mainapp/index.html', {'categoriess': categories,'itemss':items})
--- shoppers/urls.py
+++ shoppers/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from mainapp.views import index
 
 urlpatterns = [
     path('',index, name='index'),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
