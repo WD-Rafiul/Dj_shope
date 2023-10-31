14a71271679219b8fbc35af90412f01546c4416a
WD-Rafiul <rafiulislam331@gmail.com>
2023-11-01 00:19:45 +0600

    signup page ok

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
@@ -7,8 +7,29 @@ class SignupForm(UserCreationForm):
         model = User
         fields = ('username','email', 'password1', 'password2')
 
+    username = forms.CharField(widget=forms.TextInput(attrs={
+            'placeholder':'Enter your username',
+            'class':'w-full py-5 px-5 rounded-xl !outline-none'}))
 
-class SigninForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('email', 'password')
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+            'placeholder':'Enter your Email',
+            'class':'w-full py-5 px-5 rounded-xl !outline-none'}))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+            'placeholder':'Enter your password',
+            'class':'w-full py-5 px-5 rounded-xl !outline-none'}))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+            'placeholder':'Re-enter your password',
+            'class':'w-full py-5 px-5 roundedxl !outline-none'}))
+
+class SigninForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your user name',
+        'class': 'w-full py-4 px-6 rounded-xl !outline-none'}))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+            'placeholder':'Enter your password',
+            'class':'w-full py-5 px-5 rounded-xl !outline-none'}))
+
+
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import AuthenticationForm
 from django.urls import path
 
 from .import views
-from .forms import SignupForm
+from .forms import SigninForm
 
 
 app_name = 'mainapp'
@@ -11,5 +12,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
-    path('signin/', views.signin, name='signin'),
+    path('signin/', auth_views.LoginView.as_view(
+        template_name='mainapp/signin.html',
+        authentication_form=AuthenticationForm
+    ), name='signin'),
 ]
