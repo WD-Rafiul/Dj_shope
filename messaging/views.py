6a5470a251d2affd16ca035a5c3e088c129020b2
WD-Rafiul <rafiulislam331@gmail.com>
2023-11-30 00:13:59 +0600

    messaging add

--- messaging/views.py
+++ messaging/views.py
@@ -8,132 +8,68 @@ from .models import Conversation
 # Create your views here.
 
 @login_required
-def new_conversation(request,item_pk):
-    item = get_object_or_404(Items, pk = item_pk)
+def new_conversation(request, item_pk):
+    item = get_object_or_404(Items, pk=item_pk)
 
     if item.created_by == request.user:
-        return redirect('deshboard:index')
-
-    conversations = Conversation.objects.filter(item = item).filter(members__in = [request.user.id])
-
+        return redirect('deshboard:index') # return redirect('dashboard:index')
     
-
-    #conversations between you and the vendor
+    conversations = Conversation.objects.filter(item=item).filter(members__in=[request.user.id])
+    
+    #conversation between you and the vendo
     if conversations:
-        return redirect('deshboard:detail', pk =conversations.first().id)
+        return redirect('messaging:detail', pk=conversations.first().id)
 
     if request.method == 'POST':
         form = ConversationMessageForm(request.POST)
 
         if form.is_valid():
-            conversation = Conversation.objects.create(item= item)
+            conversation = Conversation.objects.create(item=item)
             conversation.members.add(request.user) #adding you
-            conversation.members.add(item.created_by) #adding vendor
+            conversation.members.add(item.created_by) #adding product item owner to the message
             conversation.save()
 
-            conversation_message = form.save(commit = False)
+            conversation_message = form.save(commit=False)
             conversation_message.conversation = conversation
             conversation_message.created_by = request.user
             conversation_message.save()
 
-            return redirect('item:detail', pk = item.pk)
+            return redirect('item:detail', pk=item_pk)
     else:
         form = ConversationMessageForm() #empty form
-
-    return render(request, 'messaging/new.html',{
-        'form':form
-        }) 
+    
+    return render(request, 'messaging/new.html', {
+        'form': form
+    })
 
 @login_required
 def inbox(request):
-    conversations = Conversation.objects.filter(members__in= [request.user.id])
+    conversations = Conversation.objects.filter(members__in=[request.user.id])
 
     return render(request, 'messaging/inbox.html', {
-        'conversations':conversations
-        })
+        'conversations': conversations
+    })
 
 @login_required
 def detail(request, pk):
-    conversation = Conversation.objects.filter(members__in= [request.user.id]).get(pk=pk)
+    conversation = Conversation.objects.filter(members__in=[request.user.id]).get(pk=pk)
 
     if request.method == 'POST':
         form = ConversationMessageForm(request.POST)
 
         if form.is_valid():
-            conversation_message = form.save(commit= False)
+            conversation_message = form.save(commit=False)
             conversation_message.conversation = conversation
             conversation_message.created_by = request.user
             conversation_message.save()
 
             conversation.save()
 
-            return redirect('messaging:detail', pk = pk)
+            return redirect('messaging:detail', pk=pk)
     else:
         form = ConversationMessageForm()
 
-    return render(request,'messaging/detail.html',{
-        'form':form,
-        'conversation':conversation
+    return render(request, 'messaging/detail.html', {
+        'conversation': conversation,
+        'form': form
     })
-
-# @login_required
-# def new_conversation(request, item_pk):
-#     item = get_object_or_404(Items, pk=item_pk)
-
-#     if item.created_by == request.user:
-#         return redirect('deshboard:index')
-
-#     conversations = Conversation.objects.filter(item=item, members=request.user)
-
-#     # Conversations between you and the vendor
-#     if conversations:
-#         return redirect('deshboard:detail', pk=conversations.first().id)
-
-#     if request.method == 'POST':
-#         form = ConversationMessageForm(request.POST)
-
-#         if form.is_valid():
-#             conversation = Conversation.objects.create(item=item)
-#             conversation.members.add(request.user)  # adding you
-#             conversation.members.add(item.created_by)  # adding vendor
-#             conversation.save()
-
-#             conversation_message = form.save(commit=False)
-#             conversation_message.conversation = conversation
-#             conversation_message.created_by = request.user
-#             conversation_message.save()
-
-#             return redirect('item:detail', pk=item.pk)
-#     else:
-#         form = ConversationMessageForm()
-
-#     return render(request, 'messaging/new.html', {'form': form})
-
-# @login_required
-# def inbox(request):
-#     conversations = Conversation.objects.filter(members=request.user)
-
-#     return render(request, 'messaging/inbox.html', {'conversations': conversations})
-
-# @login_required
-# def detail(request, pk):
-#     conversation = Conversation.objects.filter(members=request.user, pk=pk).first()
-
-#     if not conversation:
-#         # Handle the case where the conversation doesn't exist or the user doesn't have access
-#         return HttpResponse("Conversation not found or you don't have access.")
-
-#     if request.method == 'POST':
-#         form = ConversationMessageForm(request.POST)
-
-#         if form.is_valid():
-#             conversation_message = form.save(commit=False)
-#             conversation_message.conversation = conversation
-#             conversation_message.created_by = request.user
-#             conversation_message.save()
-
-#             return redirect('messaging:detail', pk=pk)
-#     else:
-#         form = ConversationMessageForm()
-
-#     return render(request, 'messaging/detail.html', {'form': form, 'conversation': conversation})
